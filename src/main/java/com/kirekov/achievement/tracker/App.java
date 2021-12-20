4c55ab661c33d74a2f477f5ede72ffcd29a0c656
Semyon Kirekov <semyon@kirekov.com>
2021-12-20 17:45:35 +0300

    #1 rename App class

--- src/main/java/com/kirekov/achievement/tracker/App.java
+++ src/main/java/com/kirekov/achievement/tracker/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AchievementsTrackerTelegramBotApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AchievementsTrackerTelegramBotApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
