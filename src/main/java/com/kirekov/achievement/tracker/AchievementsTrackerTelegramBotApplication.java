0abaa78194ce4ae4cadd9807e4510c6a08669095
Semyon Kirekov <semyon@kirekov.com>
2021-12-20 17:44:04 +0300

    #1 init spring boot app

---
+++ src/main/java/com/kirekov/achievement/tracker/AchievementsTrackerTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.kirekov.achievement.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchievementsTrackerTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AchievementsTrackerTelegramBotApplication.class, args);
+	}
+
+}
