5951345de635ddf19154139dec9d51d4916f32f9
Semyon Kirekov <semyon@kirekov.com>
2021-12-20 17:46:22 +0300

    #1 rename AppTest

--- src/test/java/com/kirekov/achievement/tracker/AppTest.java
+++ src/test/java/com/kirekov/achievement/tracker/AppTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AchievementsTrackerTelegramBotApplicationTests {
+class AppTest {
 
 	@Test
 	void contextLoads() {
