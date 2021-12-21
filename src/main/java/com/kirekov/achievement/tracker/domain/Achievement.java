ebcfd57e812cf49e5014956c9b1e400b55453eec
Semyon Kirekov <semyon@kirekov.com>
2021-12-21 14:37:40 +0300

    #6 add Achievement and User entities

---
+++ src/main/java/com/kirekov/achievement/tracker/domain/Achievement.java
@@ -0,0 +1,89 @@
+package com.kirekov.achievement.tracker.domain;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+
+import java.time.OffsetDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "achievement")
+public class Achievement {
+
+  public static Achievement newAchievement(String name, String description) {
+    return newAchievement(name, description, null);
+  }
+
+  public static Achievement newAchievement(String name, String description, User userWhoCreated) {
+    final var achievement = new Achievement();
+    achievement.setName(name);
+    achievement.setDescription(description);
+    achievement.setUserWhoCreated(userWhoCreated);
+    return achievement;
+  }
+
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  @Column(name = "achievement_id")
+  private Long id;
+
+  @NotNull(message = "Achievement name cannot be null")
+  private String name = "";
+
+  @NotNull(message = "Achievement description cannot be null")
+  private String description = "";
+
+  @Column(name = "user_id_who_created", updatable = false)
+  @ManyToOne(fetch = LAZY)
+  @NotNull(message = "User who created achievement cannot be null")
+  private User userWhoCreated;
+
+  @Column(name = "date_created", updatable = false)
+  @NotNull(message = "Achievement date created cannot be null")
+  private OffsetDateTime dateCreated = OffsetDateTime.now();
+
+  protected Achievement() {
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public User getUserWhoCreated() {
+    return userWhoCreated;
+  }
+
+  public OffsetDateTime getDateCreated() {
+    return dateCreated;
+  }
+
+  void setName(String name) {
+    this.name = name;
+  }
+
+  void setDescription(String description) {
+    this.description = description;
+  }
+
+  void setUserWhoCreated(User userWhoCreated) {
+    this.userWhoCreated = userWhoCreated;
+  }
+
+  void setDateCreated(OffsetDateTime dateCreated) {
+    this.dateCreated = dateCreated;
+  }
+}
---
+++ src/main/java/com/kirekov/achievement/tracker/domain/User.java
@@ -0,0 +1,166 @@
+package com.kirekov.achievement.tracker.domain;
+
+
+import static java.util.Objects.requireNonNullElse;
+import static javax.persistence.CascadeType.PERSIST;
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import org.hibernate.annotations.NaturalId;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+  public static User newRealUser(long uniqueIdentifier, String firstName) {
+    return newRealUser(uniqueIdentifier, firstName, "", "");
+  }
+
+  public static User newRealUser(
+      long uniqueIdentifier,
+      String firstName,
+      String lastName,
+      String username
+  ) {
+    final var user = new User();
+    user.setUniqueIdentifier(uniqueIdentifier);
+    user.setFirstName(firstName);
+    user.setLastName(lastName);
+    user.setUsername(username);
+    user.setBot(false);
+    return user;
+  }
+
+  public static User newBot(long uniqueIdentifier, String firstName) {
+    final var user = new User();
+    user.setBot(true);
+    user.setUniqueIdentifier(uniqueIdentifier);
+    user.setFirstName(firstName);
+    return user;
+  }
+
+  protected User() {
+  }
+
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  @Column(name = "user_id")
+  private Long id;
+
+  @NaturalId
+  @Column(name = "unique_identifier", updatable = false, unique = true)
+  @NotNull(message = "User unique identifier cannot be null")
+  private long uniqueIdentifier;
+
+  @Column(name = "is_bot", updatable = false)
+  @NotNull(message = "User bot flag cannot be null")
+  private boolean isBot;
+
+  @Column(name = "date_registered", updatable = false)
+  @NotNull(message = "User date registered cannot be null")
+  private OffsetDateTime dateRegistered = OffsetDateTime.now();
+
+  @Column(name = "first_name")
+  @NotNull(message = "User first name cannot be null")
+  private String firstName;
+
+  @Column(name = "last_name")
+  private String lastName = "";
+
+  @Column(name = "username", updatable = false)
+  private String username = "";
+
+  @OneToMany(fetch = LAZY, mappedBy = "userWhoCreated", cascade = PERSIST)
+  private List<Achievement> achievements = new ArrayList<>();
+
+  public void addAchievement(Achievement achievement) {
+    achievement.setUserWhoCreated(this);
+    achievements.add(achievement);
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public long getUniqueIdentifier() {
+    return uniqueIdentifier;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public boolean isBot() {
+    return isBot;
+  }
+
+  public OffsetDateTime getDateRegistered() {
+    return dateRegistered;
+  }
+
+  public String getLastName() {
+    return requireNonNullElse(lastName, "");
+  }
+
+  public String getUsername() {
+    return requireNonNullElse(username, "");
+  }
+
+  public List<Achievement> getAchievements() {
+    return achievements;
+  }
+
+  void setUniqueIdentifier(long uniqueIdentifier) {
+    this.uniqueIdentifier = uniqueIdentifier;
+  }
+
+  void setBot(boolean bot) {
+    isBot = bot;
+  }
+
+  void setDateRegistered(OffsetDateTime dateRegistered) {
+    this.dateRegistered = dateRegistered;
+  }
+
+  void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  void setUsername(String username) {
+    this.username = username;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    User user = (User) o;
+    return uniqueIdentifier == user.uniqueIdentifier;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(uniqueIdentifier);
+  }
+}
