fcfd977750b5d3c71a2679b30c3cbf5c760ef829
Semyon Kirekov <semyon@kirekov.com>
2021-12-21 23:44:11 +0300

    #6 fix DBTest

--- src/test/java/com/kirekov/achievement/tracker/test_util/DBTest.java
+++ src/test/java/com/kirekov/achievement/tracker/test_util/DBTest.java
@@ -1,9 +1,11 @@
 package com.kirekov.achievement.tracker.test_util;
 
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
 import static org.springframework.transaction.annotation.Propagation.NOT_SUPPORTED;
 
 import java.lang.annotation.Retention;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Retention(RUNTIME)
 @DataJpaTest(showSql = false)
 @Transactional(propagation = NOT_SUPPORTED)
+@AutoConfigureTestDatabase(replace = NONE)
 @Import(TestDbFacade.class)
 public @interface DBTest {
 
