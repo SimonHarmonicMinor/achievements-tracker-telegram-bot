d3f8e401e2a736708cfa8f8999b85edd51edc32b
Semyon Kirekov <semyon@kirekov.com>
2021-12-22 22:36:50 +0300

    #12 add AchievementCreateService + test

---
+++ src/main/java/com/kirekov/achievement/tracker/service/command/AchievementCreateService.java
@@ -0,0 +1,38 @@
+package com.kirekov.achievement.tracker.service.command;
+
+import com.kirekov.achievement.tracker.domain.Achievement;
+import com.kirekov.achievement.tracker.exception.UserNotFoundException;
+import com.kirekov.achievement.tracker.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class AchievementCreateService {
+
+  private final UserRepository userRepository;
+
+  public AchievementCreateService(
+      UserRepository userRepository
+  ) {
+    this.userRepository = userRepository;
+  }
+
+  /**
+   * Create new achievement.
+   *
+   * @param command achievement info
+   * @throws UserNotFoundException is user is not found
+   */
+  @Transactional
+  public void createAchievement(CreateAchievementCommand command) {
+    final var user =
+        userRepository.findById(command.getUserId())
+            .orElseThrow(() -> new UserNotFoundException(command.getUserId()));
+    final var achievement = Achievement.newAchievement(
+        command.getAchievementName(),
+        command.getAchievementDescription()
+    );
+    user.addAchievement(achievement);
+    userRepository.save(user);
+  }
+}
