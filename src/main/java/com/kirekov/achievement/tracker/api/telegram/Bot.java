ec834651c9d3429de8b4322e2c19eaaa0d85394d
Semyon Kirekov <semyon@kirekov.com>
2021-12-23 13:03:48 +0300

    #14 add Bot component

---
+++ src/main/java/com/kirekov/achievement/tracker/api/telegram/Bot.java
@@ -0,0 +1,50 @@
+package com.kirekov.achievement.tracker.api.telegram;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class Bot extends TelegramLongPollingBot {
+
+  private static final Logger log = LoggerFactory.getLogger(Bot.class);
+
+  @Value("${bot.token}")
+  private String token;
+  @Value("${bot.username}")
+  private String username;
+
+
+  @Override
+  public String getBotUsername() {
+    return username;
+  }
+
+  @Override
+  public String getBotToken() {
+    return token;
+  }
+
+  @Override
+  public void onRegister() {
+    log.info("Telegram bot '{}' has been registered", username);
+  }
+
+  @Override
+  public void onUpdateReceived(Update update) {
+    if (!update.hasMessage()) {
+      return;
+    }
+    final var message = update.getMessage();
+    try {
+      execute(new SendMessage(String.valueOf(message.getChatId()), "Stub message"));
+    } catch (TelegramApiException e) {
+      log.error("Failed to send message to " + message.getChat(), e);
+    }
+  }
+}
